86e76be1e211742a4e4b3ac63bbce800dd0a1dd9
saim514 <81713694+saim514@users.noreply.github.com>
2022-06-03 17:35:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+song = "";
+
 function setup()
 {
-    canvas = createCanvas(600, 600);
+    canvas = createCanvas(600, 500);
+
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
 }
